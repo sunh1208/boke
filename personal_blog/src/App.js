c4efa0876a3e9e0d6923b6dab87d3f982779046e
sunhh <sunh1208@163.com>
2019-08-18 11:38:41 +0800

    mergewang

--- personal_blog/src/App.js
+++ personal_blog/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import BlogList from "./views/page/blogList/blogList.js"
 import Blogparticulars from './views/page/blogparticulars/blogparticulars.jsx'
 function App() {
   return (
     <div className="App">
-    <Blogparticulars></Blogparticulars>    
-  
+      <Blogparticulars></Blogparticulars>
     </div>
   );
 }
--- personal_blog/src/component/BlogItem/index.js
+++ personal_blog/src/component/BlogItem/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-
+import "./index.scss"
 export default class BlogItem extends Component {
     render() {
         // let { item } = this.props
         return (
-            <div>
+            <div className="blogItem_item">
                 <div className="bolg_item_img">
                     <img src="; " alt="; " />
                 </div>
                 <div className="blog_item_contont">
                     <h3>你好呀</h3>
+                    <p>
+                        阿西吧是是是嘻嘻嘻啊啊啊啊从啛啛喳喳错行者常至出现在现场直线轴承擦擦擦svdsvdvbwavacsAa是大V啊啊吃撒擦
+                    </p>
+                    <div className="blog_span_box">
+                    </div>
                 </div>
             </div>
         )
