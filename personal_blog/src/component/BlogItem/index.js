9ddce54300467c8398d43f3c401a5323f1281f52
sunhh <sunh1208@163.com>
2019-08-18 19:58:12 +0800

    8-18

---
+++ personal_blog/src/component/BlogItem/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class BlogItem extends Component {
+    render() {
+        // let { item } = this.props
+        return (
+            <div>
+                <div className="bolg_item_img">
+                    <img src="; " alt="; " />
+                </div>
+                <div className="blog_item_contont">
+
+                </div>
+            </div>
+        )
+
+    }
+}
